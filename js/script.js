486423e6e7aef72595b04f1961a8800c6f7af6d0
Michael Corcias <michael.corcias@gelt.com>
2024-11-19 22:39:52 +0100

    version2

--- js/script.js
+++ js/script.js
@@ -1,25 +1,20 @@
 const btn = document.getElementById("btnReestablecer")
 const txtVisitas = document.getElementById("contadorVisitas")
-let contador = 
-    localStorage.getItem("visitas") 
-    ? parseInt(localStorage.getItem("visitas")) 
-    : 0 
 
-sumarVisita()
+document.addEventListener("DOMContentLoaded", () => {
+    let contador = parseInt(localStorage.getItem("visitas")) || 0
 
-function sumarVisita() {
-    contador += 1
-    txtVisitas.innerText = contador
-}
+    sumarVisita()
 
-localStorage.setItem("visitas", contador)
+    function sumarVisita() {
+        contador += 1
+        txtVisitas.innerText = contador
+        localStorage.setItem("visitas", contador)
+    }
 
-
-console.log(localStorage);
-
-
-btn.addEventListener("click", () => {
-    localStorage.clear()
-    console.log(localStorage);
-    txtVisitas.innerText = 0
+    btn.addEventListener("click", () => {
+        contador = 0
+        localStorage.clear()
+        txtVisitas.innerText = contador
+    })
 })
